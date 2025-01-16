0a562c778da533693ac13d4ecc61e319e7b48b86
rangga <infinity.duos@gmail.com>
2025-01-16 13:18:35 +0700

    fix express error

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
-import { VersioningType } from '@nestjs/common';
+import express, { Express } from 'express';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import setupSwagger from './utils/setup-swagger.util';
 
-// This function will create a serverless-compatible handler
+const server: Express = express();
 async function bootstrap() {
   const server = express();
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
@@ -15,14 +15,12 @@ async function bootstrap() {
     defaultVersion: '1',
   });
 
+  app.useGlobalPipes(new ValidationPipe());
   setupSwagger(app);
 
   // Return the HTTP server to Vercel
   return app;
 }
 
-// Export the serverless handler for Vercel
-export default async (req, res) => {
-  const app = await bootstrap();
-  return app.getHttpServer()(req, res); // Delegate the request to NestJS app
-};
+bootstrap();
+export default server;
